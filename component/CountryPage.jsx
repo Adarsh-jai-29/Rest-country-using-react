3cd4a9b945f90ccf1989c65095ae3fe15305e954
adarsh <adarshjai29@gmail.com>
2024-07-18 10:21:04 +0530

    add shimmer effect at Landing  page

--- component/CountryPage.jsx
+++ component/CountryPage.jsx
@@ -45,79 +45,79 @@ console.log(borderCountries)
   if (error) return <div>Error loading country data.</div>;
 
   return (
-    <>
-      {data && (
-        <main>
-          <div className="country-details-container">
-            <span className="back-button" onClick={() => history.back()}>
-              <i className="fa-solid fa-arrow-left"></i>&nbsp; Back
-            </span>
-            <div className="country-details">
-              <img src={data.flags.svg} alt={data.name.common} />
-              <div className="details-text-container">
-                <h1>{data.name.common}</h1>
-                <div className="details-text">
-                  <p>
-                    <b>Native Name: </b>
-                    <span className="native-name">
-                      {Object.values(data.name.nativeName)[0].common}
-                    </span>
-                  </p>
-                  <p>
-                    <b>Population: </b>
-                    <span className="population">{data.population}</span>
-                  </p>
-                  <p>
-                    <b>Region: </b>
-                    <span className="region">{data.region}</span>
-                  </p>
-                  <p>
-                    <b>Sub Region: </b>
-                    <span className="sub-region">{data.subregion}</span>
-                  </p>
-                  <p>
-                    <b>Capital: </b>
-                    <span className="capital">{data.capital.join(", ")}</span>
-                  </p>
-                  <p>
-                    <b>Top Level Domain: </b>
-                    <span className="top-level-domain">{data.tld}</span>
-                  </p>
-                  <p>
-                    <b>Currencies: </b>
-                    <span className="currencies">
-                      {Object.values(data.currencies)[0].name}
-                    </span>
-                  </p>
-                  <p>
-                    <b>Languages: </b>
-                    <span className="languages">
-                      {Object.values(data.languages).join(", ")}
-                    </span>
-                  </p>
-                </div>
-                <div className="border-countries">
-                  <b>Border Countries: </b>&nbsp;
-                  {data.borders == undefined ? (
-                    <div>No Border Country</div>
-                  ) : (
-                    borderCountries.map((borderCountry) => (
-                      <Link
-                        key={borderCountry.cca3}
-                        to={`/${borderCountry.name.common}`}
-                      >
-                        {borderCountry.name.common}
-                      </Link>
-                    ))
-                  )}
-                </div>
-              </div>
-            </div>
-          </div>
-        </main>
-      )}
-    </>
+  <>
+  {data && (
+<main>
+<div className="country-details-container">
+  <span className="back-button" onClick={() => history.back()}>
+    <i className="fa-solid fa-arrow-left"></i>&nbsp; Back
+  </span>
+  <div className="country-details">
+    <img src={data.flags.svg} alt={data.name.common} />
+    <div className="details-text-container">
+      <h1>{data.name.common}</h1>
+      <div className="details-text">
+        <p>
+          <b>Native Name: </b>
+          <span className="native-name">
+            {Object.values(data.name.nativeName)[0].common}
+          </span>
+        </p>
+        <p>
+          <b>Population: </b>
+          <span className="population">{data.population}</span>
+        </p>
+        <p>
+          <b>Region: </b>
+          <span className="region">{data.region}</span>
+        </p>
+        <p>
+          <b>Sub Region: </b>
+          <span className="sub-region">{data.subregion}</span>
+        </p>
+        <p>
+          <b>Capital: </b>
+          <span className="capital">{data.capital.join(", ")}</span>
+        </p>
+        <p>
+          <b>Top Level Domain: </b>
+          <span className="top-level-domain">{data.tld}</span>
+        </p>
+        <p>
+          <b>Currencies: </b>
+          <span className="currencies">
+            {Object.values(data.currencies)[0].name}
+          </span>
+        </p>
+        <p>
+          <b>Languages: </b>
+          <span className="languages">
+            {Object.values(data.languages).join(", ")}
+          </span>
+        </p>
+      </div>
+      <div className="border-countries">
+        <b>Border Countries: </b>&nbsp;
+        {data.borders == undefined ? (
+          <div>No Border Country</div>
+        ) : (
+          borderCountries.map((borderCountry) => (
+            <Link
+              key={borderCountry.cca3}
+              to={`/${borderCountry.name.common}`}
+            >
+              {borderCountry.name.common}
+            </Link>
+          ))
+        )}
+      </div>
+    </div>
+  </div>
+</div>
+</main>
+  )}
+  </>
   );
-}
+  }
 
 export default CountryPage;

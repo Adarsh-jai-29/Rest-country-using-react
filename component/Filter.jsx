74becdf0b71a1520518ca18160031fa7ed35f950
adarsh <adarshjai29@gmail.com>
2024-08-12 10:18:25 +0530

    implement filter functionalty

--- component/Filter.jsx
+++ component/Filter.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 export default function Filter() {
+  const [region,setRegion] = useState('')
+  const selectedValue = (e)=>{console.log(e.target.value)}
   return (
-    <select className="filter-by-region">
+    <form>
+    <select className="filter-by-region" onChange={selectedValue}>
     <option hidden>Filter by Region</option>
     <option value="Africa">Africa</option>
     <option value="America">America</option>
@@ -10,5 +14,6 @@ export default function Filter() {
     <option value="Europe">Europe</option>
     <option value="Oceania">Oceania</option>
   </select>
+  </form>
   )
 }
